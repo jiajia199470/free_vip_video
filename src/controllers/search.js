2a1b684fdec58294fc968fa970db7be54c4c1498
zhangxj <1083573413@qq.com>
2019-03-02 21:54:54 +0800

    git init

---
+++ src/controllers/search.js
@@ -0,0 +1,28 @@
+const TvList = require('../models').TvList
+const Movie = require('../models').Movie
+
+const videoSearch = async function(ctx) {
+  var searchValue = ctx.query.value
+    console.log(searchValue);
+  var movieSearchResult = await Movie.findOne({
+    where: {
+      name: searchValue
+    }
+  })
+  var tvSearchResult = await TvList.findOne({
+    where: {
+      name: searchValue
+    }
+  })
+  ctx.body = {
+    message: '成功',
+    success: true,
+    data: {
+      tv: tvSearchResult,
+      movie: movieSearchResult
+    }
+  }
+}
+module.exports = {
+  videoSearch
+}
