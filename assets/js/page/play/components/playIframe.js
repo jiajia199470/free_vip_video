79d30bfb649d1da4e2c583f9669597473b7ba4a4
zhangxj <1083573413@qq.com>
2019-03-07 11:42:31 +0800

    调整剧集播放页面样式/屏蔽电影播放第三方植入广告

--- assets/js/page/play/components/playIframe.js
+++ assets/js/page/play/components/playIframe.js
@@ -1,36 +1,40 @@
-(function(window, $) {
-  var  baseUrl = 'https://www.myxin.top/jx/api/?url='
-  var videoUrl = $('#vplay').attr('data-src');
-  var playUrl = '';
-  var $vplay = $('#vplay')
-  var piframe = {
-    init: function() {
-      var self = this;
-      console.log($('.api:first'))
-      self.changePort()
-      self.videoPlay()
-    },
-    videoPlay: function() {
-      $('.api:first').trigger('click')
-    },
-    changePort: function() {
-      $(document).on('click', '.api', function() {
-        var me = $(this);
-        var uid = me.find('input').attr('id')
-          if(+uid === 1) {
-              baseUrl = 'https://www.myxin.top/jx/api/?url='
-          } else if(+uid === 2) {
-              baseUrl = 'https://jx.wslmf.com/?url='
-          } else if (+uid === 3) {
-              baseUrl = 'https://api.bbbbbb.me/jx/?url='
-          }
-        $vplay.attr('src', baseUrl + videoUrl);
-        $('.api').each(function() {
-          $(this).removeClass('active')
-        })
-        me.addClass('active')
-      })
-    }
-  }
-  piframe.init();
-})(window, jQuery)
+(function (window, $) {
+    var baseUrl = 'https://www.myxin.top/jx/api/?url=';
+    var videoUrl = $('#vplay').attr('data-src');
+    var playUrl = '';
+    var $vplay = $('#vplay');
+    var piframe = {
+        init: function () {
+            var self = this;
+            console.log($('.api:first'));
+            self.changePort();
+            self.videoPlay();
+
+            if($(window).width()<=544){
+                $('#ckvip').css('height','200px');
+            }
+        },
+        videoPlay: function () {
+            $('.api:first').trigger('click');
+        },
+        changePort: function () {
+            $(document).on('click', '.api', function () {
+                var me = $(this);
+                var uid = me.find('input').attr('id');
+                if (+uid === 1) {
+                    baseUrl = 'https://www.myxin.top/jx/api/?url=';
+                } else if (+uid === 2) {
+                    baseUrl = 'https://jx.wslmf.com/?url=';
+                } else if (+uid === 3) {
+                    baseUrl = 'https://api.bbbbbb.me/jx/?url=';
+                }
+                $vplay.attr('src', baseUrl + videoUrl);
+                $('.api').each(function () {
+                    $(this).removeClass('active');
+                });
+                me.addClass('active');
+            });
+        },
+    };
+    piframe.init();
+})(window, jQuery);
